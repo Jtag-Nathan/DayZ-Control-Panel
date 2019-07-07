08f9750a4e4737a0f0f719df861e560e0e40ba57
Nathan James <nathanjames001@hotmail.co.uk>
2019-07-07 23:33:08 +0100

    Adding JWT and bcrypt to server

--- server/server.js
+++ server/server.js
@@ -1,21 +1,9 @@
 // Require packages and set the port
 const express = require('express');
 const bodyParser = require('body-parser');
-const routes = require('./routes/routes');
 const port = 3000;
 const app = express();
+const router = express.Router();
 
-// Start the server
-const server = app.listen(port, (error) => {
-    if (error) return console.log(`Error: ${error}`);
- 
-    console.log(`Server listening on port ${server.address().port}`);
-});
-
-// Use Node.js body parsing middleware
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-    extended: true,
-}));
-
-routes(app);
+router.use(bodyParser.urlencoded({extended: false}));
+router.use(bodyParser.json());
