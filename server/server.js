d2a66ac5b48c90f7257f8b9707b0178f14e9ba3b
Nathan James <nathanjames001@hotmail.co.uk>
2019-07-08 04:22:44 +0100

    Removed server

--- server/server.js
+++
@@ -1,9 +0,0 @@
-// Require packages and set the port
-const express = require('express');
-const bodyParser = require('body-parser');
-const port = 3000;
-const app = express();
-const router = express.Router();
-
-router.use(bodyParser.urlencoded({extended: false}));
-router.use(bodyParser.json());
